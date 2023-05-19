775c17f929487c4ceeb5a9d04f8a79f25a653442
d2gex <danielgguk@gmail.com>
2023-05-19 16:25:22 +0200

    Add column rename logic to dataframe

--- R/data_filter.R
+++ R/data_filter.R
@@ -1,4 +1,6 @@
 library("R6")
+library("data.table")
+
 
 DbDataFilter <- R6Class("DbDataFilter",
                         public = list(
@@ -7,7 +9,7 @@ DbDataFilter <- R6Class("DbDataFilter",
                             self$db_data <- df
                           },
                           rename_columns = function(old_cols, new_cols) {
-                            self$db_data <- self$db_data %>% rename(setNames(old_cols, new_cols))
+                            data.table::setnames(self$db_data, old=old_cols, new=new_cols)
                             invisible(self)
                           }
                         ))
--- R/main.R
+++ R/main.R
@@ -1,20 +1,25 @@
 source("config.R")
 source("data_filter.R")
 library("tidyverse")
+library("assertr")
 
 read_db_details <- function(path_to_csv) {
   if (!DB_READ) {
     utpb_original_db <- read.csv(path_to_csv)
     DB_READ <- TRUE
   }
-
-  return (utpb_original_db)
+  return(utpb_original_db)
 }
 
-run <- function() {
-  db_data <- read_db_details(DB_TALLAS_PATH)
-  # db_filer <- DbDataFilter$new(db_data)
-  # db_filer$rename_columns(c)
+run <- function(db_data) {
+
+  db_filer <- DbDataFilter$new(db_data)
+  old_columns <- names(COL_NAME_MAPPING)
+  new_columns <- unname(COL_NAME_MAPPING)
+  db_filer$rename_columns(old_columns, unlist(new_columns))
+  db_filer$db_data %>% verify(do.call(has_all_names, new_columns))
+  return(db_filer$db_data)
 }
 
-run()
+db_data <- read_db_details(DB_TALLAS_PATH)
+clean_db_data <- run(db_data)
