c6c7db742604dcb092402bac7a0d8dc9afdcea3c
d2gex <danielgguk@gmail.com>
2023-05-23 09:27:26 +0200

    Add string to posix datatime object method

--- R/data_filter.R
+++ R/data_filter.R
@@ -8,7 +8,7 @@ library("tidyverse")
 DbDataFilter <- R6Class("DbDataFilter",
                         public = list(
                           db_data = NULL,
-                          clean_df = data.frame(),
+                          clean_df = NULL,
                           dirty_df = data.frame(),
                           initialize = function(df) {
                             self$db_data <- df
@@ -19,18 +19,20 @@ DbDataFilter <- R6Class("DbDataFilter",
                             invisible(self)
                           },
                           to_datetime = function(fields) {
-                            self$clean_df <- self$clean_df %>% mutate_at(.vars = fields, .funs = dmy_hms)
+                            self$clean_df <- self$clean_df %>% mutate_at(.vars = fields,
+                                                                         ~ as.POSIXct(., format = "%d/%m/%Y %H:%M", tz = "UTC"))
                             invisible(self)
                           },
                           get_rid_of_NaNs = function(fields) {
-                            self$clean_df <- private$copy_or_add(self$clean_df, self$db_data %>% drop_na(any_of(fields)))
-                            self$dirty_df <- private$copy_or_add(self$dirty_df, self$db_data %>% filter(if_any(fields, ~is.na(.x))))
+                            dirty_data <- self$clean_df %>% filter(if_all(fields, ~is.na(.x)))
+                            self$dirty_df <- private$copy_or_add(self$dirty_df, dirty_data)
+                            self$clean_df <- self$clean_df %>% filter(if_all(fields, ~!is.na(.x)))
                             invisible(self)
                           }
                         ),
                         private = list(
                           copy_or_add = function(df_to, df_from) {
-                            if (nrow(df_to)) {
+                            if (!nrow(df_to)) {
                               df_to <- df_from
                             }
                             else {
--- R/main.R
+++ R/main.R
@@ -1,6 +1,7 @@
 source("config.R")
 source("data_filter.R")
 library("tidyverse")
+library("testit")
 library("assertr")
 
 read_db_details <- function(path_to_csv) {
@@ -11,31 +12,32 @@ read_db_details <- function(path_to_csv) {
   return(utpb_original_db)
 }
 
-run <- function(db_data) {
-
-  db_filter <- DbDataFilter$new(db_data)
-
-  # (1) Rename georeference columns
-  col_name_mapping <- list(
-    'LON.inicio' = 'start_long',
-    'LAT.inicio' = 'start_lat',
-    'LON.final' = 'end_long',
-    'LAT.final' = 'end_lat'
-  )
-  old_columns <- names(col_name_mapping)
-  new_columns <- unname(col_name_mapping)
-  db_filter$rename_columns(old_columns, unlist(new_columns))
-  # db_filter$clean_df %>% verify(do.call(has_all_names, new_columns))
-
-  # (2) Convert all potential datetime columns as such
-  datetime_columns <- c('dia', 'FLARG', 'FVIR', 'HorafL', 'HorafV')
-  db_filter$to_datetime(datetime_columns)
-
-  # (3) Extract NaN values for dia, largada and virada columns
-  # db_filter$split_df_by_blank_fields(datetime_columns)
-
-  return(db_filter$clean_df)
-}
 
 db_data <- read_db_details(DB_TALLAS_PATH)
-clean_db_data <- run(db_data)
+db_filter <- DbDataFilter$new(db_data)
+
+# (1) Rename georeference columns
+col_name_mapping <- list(
+  'LON.inicio' = 'start_long',
+  'LAT.inicio' = 'start_lat',
+  'LON.final' = 'end_long',
+  'LAT.final' = 'end_lat'
+)
+old_columns <- names(col_name_mapping)
+new_columns <- unname(col_name_mapping)
+db_filter$rename_columns(old_columns, unlist(new_columns))
+mute <- db_filter$clean_df %>% verify(do.call(has_all_names, new_columns))
+
+# (2) Convert all potential datetime columns as such
+datetime_columns <- c('dia', 'FLARG', 'FVIR', 'HorafL', 'HorafV')
+db_filter$to_datetime(datetime_columns)
+
+# (3) Get rid of rows that eith HorafL or FLARG are null
+# largarda_columns <- c('FLARG', 'HorafL')
+# db_filter$get_rid_of_NaNs(largarda_columns)
+# mute <- db_filter$dirty_df %>%
+#   verify(is.na(FLARG)) %>%
+#   verify(is.na(HorafL))
+# testit::assert("dirty + clean != data",
+#                nrow(db_filter$db_data) == nrow(db_filter$clean_df) + nrow(db_filter$dirty_df))
+
