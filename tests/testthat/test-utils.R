45868143287d067c51a6708b41bccd9f8df831bc
d2gex <danielgguk@gmail.com>
2023-05-31 21:24:31 +0200

    Add tests for make_cols_same_length function

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,4 +1,28 @@
 source("../../R/utils.R")
-test_that("multiplication works", {
-  expect_equal(2 * 2, 5)
+library("data.table")
+
+test_that("Balance out INTEGER dataframe with different length", {
+  df_x <- data.frame(
+    integer = c(1, 2, 3)
+  )
+  df_y <- data.frame(
+    integer = c(1, 2, 3, 4)
+  )
+  fill <- -Inf
+  results <- make_cols_same_length(df_x$integer, df_y$integer, fill)
+  expect_equal(results$col_1, c(df_x$integer, fill))
+  expect_equal(results$col_2, df_y$integer)
+})
+
+test_that("Balance out STRING dataframe with different length", {
+  df_x <- data.frame(
+    str = c('str1', 'str2', 'str3')
+  )
+  df_y <- data.frame(
+    str = c('str1', 'str2')
+  )
+  fill <- '-Inf'
+  results <- make_cols_same_length(df_x$str, df_y$str, fill)
+  expect_equal(results$col_1, df_x$str)
+  expect_equal(results$col_2, c(df_y$str, fill))
 })
