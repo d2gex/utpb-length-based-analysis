140745c3a56ba446aa1a8cd8d7645b50ff323c70
d2gex <dgarcia@iim.csic.es>
2023-07-11 19:06:16 +0100

    Start constructing catches query with especies_seleccionadas

--- R/db/new_tallas.sql
+++ R/db/new_tallas.sql
@@ -1,38 +1,90 @@
-SELECT ob.[Observador 1]
-    j.idjornada,
-    j.dia,
-    z2.ZONA,
-    l.Idlance,
-    l.FLARG,
-    l.HorafL,
-    l.FVIR,
-    l.HorafV,
-    l.mcarte1,
-    l.[LAT inicio],
-    l.[LON inicio],
-    l.[LAT final],
-    l.[LON final],
-    l.TIPOFONDO,
-    l.PROFMax,
-    l.PROFMin,
-    l.PROFMed,
-    cax.ARTE,
-    em.Idartes,
-    uem.idespecie,
-    uem.ESPECIE,
-    m.CoD,
-    m.NUMINDIVS,
-    m.PESO,
-    m.TALLA
+SELECT count(*)
 FROM
     ((((((((Jornada j INNER JOIN [Observador 1] ob on j.OBSER1 = ob.Id)
     INNER JOIN FLOTA f ON f.Idflota = j.Idflota)
+    INNER JOIN CDTOTS as cap ON cap.idjornada = j.idjornada)
     INNER JOIN zonas2 z2 ON z2.Idzona = j.Idzona2)
     INNER JOIN Lances l ON l.idjorn = j.idjornada)
     INNER JOIN Artes ar ON ar.Idlance = l.Idlance)
     INNER JOIN codigos_xunta cax ON cax.Idarte = ar.Idarte)
     INNER JOIN EspeciesM em ON em.Idartes = ar.Idnasa)
-    INNER JOIN unique_especies_seleccionadas uem ON uem.idespecie = em.IdespM)
-    INNER JOIN Muestreos m
-ON m.Idesp = em.Idesp
+    INNER JOIN unique_especies_seleccionadas uem ON uem.idespecie = em.IdespM
 WHERE j.dia >= #01/01/1999# and j.dia <=#21/12/2017#;
+
+
+SELECT distinct l.Idlance
+FROM
+    (((((((Jornada j INNER JOIN [Observador 1] ob on j.OBSER1 = ob.Id)
+    INNER JOIN FLOTA f ON f.Idflota = j.Idflota)
+    INNER JOIN zonas2 z2 ON z2.Idzona = j.Idzona2)
+    INNER JOIN Lances l ON l.idjorn = j.idjornada)
+    INNER JOIN Artes ar ON ar.Idlance = l.Idlance)
+    INNER JOIN codigos_xunta cax ON cax.Idarte = ar.Idarte)
+    INNER JOIN CDTOTS as cap ON (cap.idjornada = j.idjornada) and (cap.id_arte = cax.idarte))
+    INNER JOIN unique_especies_seleccionadas uem ON uem.idespecie = cap.IdespT
+WHERE j.dia >= #01/01/1999# and j.dia <=#21/12/2017#;
+
+SELECT  ss.idespecie,
+        ss.ESPECIE,
+        ss.ARTE,
+        catches.idjornada,
+        catches.idarte,
+        catches.Idartes,
+        catches.Idlance,
+        catches.IdespM,
+        catches.NumC,
+        catches.PesoC,
+        catches.NumD,
+        catches.PesoD
+    FROM especies_seleccionadas as ss
+        LEFT JOIN
+        (SELECT
+             j.idjornada,
+            ar.idarte,
+            l.Idlance,
+            em.Idartes,
+            em.IdespM,
+            em.NumC,
+            em.PesoC,
+            em.NumD,
+            em.PesoD
+            FROM ((((((Jornada j INNER JOIN [Observador 1] ob on j.OBSER1 = ob.Id)
+                INNER JOIN FLOTA f ON f.Idflota = j.Idflota)
+        INNER JOIN zonas2 z2 ON z2.Idzona = j.Idzona2)
+        INNER JOIN Lances l ON l.idjorn = j.idjornada)
+        INNER JOIN Artes ar ON ar.Idlance = l.Idlance)
+        INNER JOIN EspeciesM em ON em.Idartes = ar.Idnasa)
+        INNER JOIN unique_especies_seleccionadas uem on uem.idespecie = em.IdespM) as catches on catches.IdespM = ss.idespecie
+WHERE catches.Idartes in (21464379,957572841,-1657205501,-868540539,-557962433)
+order by catches.Idartes
+
+
+
+
+SELECT
+    ss.idespecie,
+    ss.ESPECIE,
+    ss.ARTE,
+    catches.idjornada,
+    catches.idarte,
+    catches.Idartes,
+    catches.Idlance,
+    catches.IdespM,
+    catches.NumC,
+    catches.PesoC,
+    catches.NumD,
+    catches.PesoD
+    FROM (((((((Jornada j INNER JOIN [Observador 1] ob on j.OBSER1 = ob.Id)
+        INNER JOIN FLOTA f ON f.Idflota = j.Idflota)
+INNER JOIN zonas2 z2 ON z2.Idzona = j.Idzona2)
+INNER JOIN Lances l ON l.idjorn = j.idjornada)
+INNER JOIN Artes ar ON ar.Idlance = l.Idlance)
+INNER JOIN EspeciesM em ON em.Idartes = ar.Idnasa)
+INNER JOIN unique_especies_seleccionadas uem on uem.idespecie = em.IdespM) as catches
+RIGHT JOIN
+        (SELECT  ss.idespecie,
+        ss.ESPECIE,
+        ss.ARTE
+         FROM especies_seleccionadas as ss) ON catches.
+WHERE catches.Idartes in (21464379,957572841,-1657205501,-868540539,-557962433)
+order by catches.Idartes
